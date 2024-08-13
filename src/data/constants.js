120a311920501a045e4809e214d0474198281e49
Sofia Mohamed Iman <sofia.iman@prodapt.com>
2024-08-13 23:44:29 +0300

    portfolio website

---
+++ src/data/constants.js
@@ -0,0 +1,472 @@
+export const Bio = {
+  name: "Sofia Iman",
+  roles: [
+    "Software Engineer",
+    "Fronted Developer",
+    "UI/UX Designer",
+    "Operations Engineer",
+    "Machine Learning Engineer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/sofiaIman",
+  resume:
+    "https://drive.google.com/file/d/1Aht9ecLiHU2aFEDaG4BjXr205k6FVBSc/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/sofia-mohamed-2258a9200/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        
+      },
+      {
+        name: "Redux",
+        
+      },
+      {
+        name: "Next Js",
+       
+      },
+      {
+        name: "HTML",
+        
+      },
+      {
+        name: "CSS",
+      },
+      {
+        name: "JavaScript",
+          
+      },
+      {
+        name: "Bootstrap",
+        
+      },
+      {
+        name: "Material UI",
+        
+      },
+      {
+        name: "Tailwind CSS",
+  
+      }
+      
+      
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+      
+      },
+      {
+        name: "Express Js",
+      
+      },
+      {
+        name: "Graph Ql",
+        
+      },
+      {
+        name: "Python",
+        
+      },
+      {
+        name: "MySQL",
+    
+      },
+      {
+        name: "Postgresql",
+        
+      },
+      {
+        name: "MongoDB",
+        
+      },
+      {
+        name: "Firebase",
+       
+      },
+      {
+        name: "PHP",
+       
+      },
+      {
+        name: "Django",
+       
+      },
+      {
+        name: "Laravel",
+       
+      },
+      {
+        name: "REST APIs",
+       
+      },
+    ],
+  },
+  {
+    title: "Machine Learning",
+skills: [
+  {
+    name: "Python",
+  },
+  {
+    name: "TensorFlow",
+  },
+  {
+    name: "Keras",
+  },
+  {
+    name: "scikit-learn",
+  },
+  {
+    name: "PyTorch",
+  },
+  {
+    name: "Pandas",
+  },
+  {
+    name: "NumPy",
+  },
+  {
+    name: "Matplotlib",
+  },
+  {
+    name: "Seaborn",
+  },
+  {
+    name: "Data Analysis",
+  },
+  {
+    name: "Data Visualization",
+  },
+  {
+    name: "Natural Language Processing (NLP)",
+  },
+  {
+    name: "Computer Vision",
+  },
+  {
+    name: "Model Deployment",
+  },
+]
+
+  },
+
+  {
+    title: "Cloud Computing",
+skills: [
+  {
+    name: "AWS",
+  },
+  {
+    name: "Azure",
+  },
+  {
+    name: "Google Cloud Platform (GCP)",
+  },
+  {
+    name: "AWS Lambda",
+  },
+  {
+    name: "Azure Functions",
+  },
+  {
+    name: "Google Cloud Functions",
+  },
+  {
+    name: "Docker",
+  },
+  {
+    name: "Kubernetes",
+  },
+  {
+    name: "CI/CD",
+  },
+  {
+    name: "Terraform",
+  },
+  {
+    name: "Ansible",
+  },
+  {
+    name: "Serverless Architecture",
+  },
+  {
+    name: "Cloud Security",
+  },
+  {
+    name: "Virtual Machines",
+  },
+  {
+    name: "Storage Solutions",
+  },
+  {
+    name: "Networking",
+  },
+]
+
+  },
+  {
+    title: "Others",
+skills: [
+  {
+    name: "DevOps",
+  },
+  {
+    name: "Version Control (Git)",
+  },
+  {
+    name: "Agile Methodologies",
+  },
+  {
+    name: "IT Support & Troubleshooting",
+  },
+  {
+    name: "Network Administration",
+  },
+  {
+    name: "Database Management",
+  },
+  {
+    name: "Technical Documentation",
+  },
+  {
+    name: "ITIL/ITSM",
+  },
+  {
+    name: "Communication & Collaboration",
+  },
+  {
+    name: "Project Management",
+  },
+  {
+    name: "UI/UX Design",
+  },
+  {
+    name: "API Integration",
+  },
+  {
+    name: "Cybersecurity",
+  },
+  {
+    name: "Virtualization",
+  },
+]
+
+  },
+];
+
+export const experiences = [
+  {
+      id: 0,
+      img: "/assets/mpesa.JPG", // Image related to M-Pesa Africa
+      role: "Operations Engineer",
+      company: "M-Pesa Africa (Contracted through Prodapt)",
+      date: "Jul 2023 - present",
+      desc: "As an Operations Engineer, contracted through Prodapt, I work with M-Pesa Africa to ensure seamless operations across various markets. Responsibilities include managing incidents, handling service requests, performing routine system monitoring and maintenance, and providing technical support. Additionally, I implement process improvements and collaborate with cross-functional teams to enhance operational efficiency.",
+      skills: [
+        "OS Support",
+        "VMware",
+        "Database Support",
+        "Network Administration",
+        "Incident Management",
+        "Monitoring Tools",
+        "ITIL/ITSM",
+        "Troubleshooting",
+        "Technical Documentation",
+        "Communication Skills",
+      ],
+    },
+
+    {
+      id: 1,
+      img: "/assets/prodapt.png", 
+      role: "Software Engineer",
+      company: "Prodapt",
+      date: "Jul 2023 - Present",
+      desc: "Working as a Software Engineer to provide services for M-Pesa Africa. Responsibilities include overseeing system operations, managing incidents, and collaborating on process improvements.",
+      skills: [
+        "Process Improvement",
+        "Communication Skills",
+        "Technical Support",
+        "Client Collaboration",
+        "Project Management",
+        "Incident Management",
+        "Automation",
+        "Software Development",
+      ],
+    },
+  
+    {
+      id: 3,
+      img: "/assets/dialafrica.jpeg", 
+      role: "Frontend Software Developer",
+      company: "Dial Afrika",
+      date: "Jul 2022 - Jan 2023",
+      desc: "Managed the software development lifecycle and integrated advanced technologies to enhance software performance. Redesigned website sections for improved user experience and visual appeal. Worked on the CRM system, chatdesk, and Call Module using React and TypeScript. Continuously improved skills in web technologies, design tools, and programming practices.",
+      skills: [
+        "Software Development Lifecycle",
+        "React",
+        "TypeScript",
+        "Frontend Development",
+        "Web Technologies",
+        "UI/UX Design",
+        "CRM Systems",
+        "Collaboration",
+        "Continuous Learning",
+      ],
+    },
+    {
+      id: 4,
+      img: "/assets/IEBC.png", 
+      role: "Contract Software Developer",
+      company: "IEBC Kenya (through Dial Afrika)",
+      date: "Aug 2022 - Sep 2022",
+      desc: "Integrated the IEBC website with a chatbot and WhatsApp to enhance user engagement during the election period. Developed and implemented a CRM and call module system in collaboration with the Dial Afrika team. Provided training for end-users on CRM functions and technical support for the IEBC call center team.",
+      skills: [
+        "Chatbot Integration",
+        "WhatsApp Integration",
+        "CRM Systems",
+        "Call Module Systems",
+        "Technical Support",
+        "User Training",
+        "Web Development",
+        "Collaboration",
+        "Problem Solving",
+      ],
+    },
+    {
+      id: 5,
+      img: "/assets/catch.png", 
+      role: "Software Engineer Intern",
+      company: "Catch Taxi Kenya",
+      date: "Jan 2021 - Mar 2021",
+      desc: "Collaborated closely with senior developers on maintaining and enhancing frontend applications. Estimated project scopes, participated in design discussions, and conducted code reviews. Designed and developed the Catch Taxi Corporate Dashboard using Figma, HTML, CSS, and JavaScript (React). Engaged in weekly tech team and company meetings to discuss project progress, design ideas, and strategies for improved efficiency.",
+      skills: [
+        "Frontend Development",
+        "Figma",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React",
+        "Project Estimation",
+        "Design Discussions",
+        "Code Reviews",
+        "Team Collaboration",
+      ],
+    },
+    {
+      id: 6,
+      img: "/assets/farsight.png", 
+      role: "IT Consultant",
+      company: "Farsight Africa Group",
+      date: "Jan 2019 - Mar 2019",
+      desc: "Provided administrative and technical support to enhance the institutional business plan and support company personnel. Offered guidance on ICT specifications for various products and services. Conducted proofreading and quality assurance for ICT bids and proposals, ensuring accuracy and compliance with requirements.",
+      skills: [
+        "Administrative Support",
+        "Technical Support",
+        "ICT Specifications",
+        "Quality Assurance",
+        "Proofreading",
+        "Business Plan Support",
+        "Bid Management",
+        "Proposal Evaluation",
+      ],
+    },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "CrimeInsight",
+    date: "Feb 2021 - Nov 2021",
+    description:
+      "This is a crime analysis and prediction system that assists the police force in identifying and analyzing patterns and trends in crime in SanFrancisco City.To accomplish analysis of crimes, K-means clustering algorithm was used in this project to identify the crime patterns which are used to commit crimes knowing the fact that each crime has certain patterns. The database was then trained with a supervised learning algorithm, Naïve Bayes to predict possible crime prone areas. Hence, the system forecasts the probability of a crime occurring in a city by analyzing the crime dataset and visualizing its findings.",
+      image:
+      "/assets/crimeInsight.jpg", 
+    tags: [
+      "Django",
+      "Machine learning Libraries",
+      "Data Visualization",
+      "K-means Clustering",
+      "Naive Bayes",
+      "Mongo DB",
+     
+    ],
+    category: "machine learning",
+    github: "https://github.com/sofiaIman/CrimeInsight",
+    webapp: "https://3-imans-bites.vercel.app/",
+  },
+  {
+    id: 0,
+    title: "StayCationer",
+    date: "Apr 2023 - May 2023",
+    description:
+      "This is a hotel booking site which helps visitors to book hotel seamlessly",
+    image:
+      "/assets/staycationer.jpg",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    category: "web app",
+    github: "https://github.com/sofiaIman/staycationer",
+    webapp: "https://staycationer.vercel.app/",
+    
+  },
+  {
+    id: 1,
+    title: "ImanBites",
+    date: "Oct 2022 - Nov 2022",
+    description:
+      "This is a fully responsive website with modern UI and UX in ReactjS. It involves use of flex and grid, CSS BEM Model, soft and pleasant animations to complex gradients. It also includes Perfectly placed media queries for satisfactory responsiveness covering almost all devices",
+    image:
+      "/assets/imanBites.jpg",
+    tags: [
+      "React Js",
+      " Responsive",
+      "UI/UX",
+      "Flex & Grid",
+      "CSS BEM Model",
+      "Animation",
+    ],
+    category: "web app",
+    github: "https://github.com/sofiaIman/3-Imans-bites",
+    webapp: "https://3-imans-bites.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Tech Pen",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "This is a fullstack blog app based on React MySql, Its use cases involve things like a user being able to log in using JWT, logging out using cookies, and how to use react MySQL to add new items, how to upload a file to a server and create and Update a post. It also covers how to fetch MySQL data, react context API and how to fetch recommended posts to a user",
+    image:
+      "/assets/techpen.jpg",
+    tags: ["ReactJs", "MySql", "Redux", "JWT", "Cookies"],
+    category: "web app",
+    github: "https://github.com/sofiaIman/tech-pen",
+    webapp: "https://3-imans-bites.vercel.app/",
+    
+  },
+  {
+    id: 3,
+    title: "Buckoid",
+    date: "Dec 2021 - Apr 2022",
+    description:
+      "This is a full stack food delivery application using Reactjs, Firebase, Framer motion, Tailwind css. This projects allows to the user to add new items and to maintain all the things over the firestore cloud database. It involves functionalities like fetching data from firebase, Filering food menu, Updating cart items and updating cart total and quantity of food added to cart",
+    image:
+      "/assets/fooddel.jpg",
+    tags: ["reactJS", "Firebase", "Framer Motion", "Tailwind CSS"],
+    category: "web app",
+    github: "https://github.com/sofiaIman/4-react-restaurant-delivery",
+    webapp: "https://3-imans-bites.vercel.app/",
+  },
+
+];
+
+
